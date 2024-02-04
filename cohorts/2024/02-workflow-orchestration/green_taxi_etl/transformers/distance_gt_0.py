20fc989e31be09f4921ed8c2d03da712fae04502
zeeros <1713464+zeeros@users.noreply.github.com>
2024-02-04 22:46:53 +0100

    Add ETL pipeline

---
+++ cohorts/2024/02-workflow-orchestration/green_taxi_etl/transformers/distance_gt_0.py
@@ -0,0 +1,16 @@
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def transform(data, *args, **kwargs):
+    return data[data['trip_distance'] > 0]
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
